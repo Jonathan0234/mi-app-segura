2a8a9228e4c78c63ff855751ca44f38c1612738e
JonathanArriaga0901 <jarriagac2@miumg.edu.gt>
2025-10-21 21:55:02 -0600

    pipeline ver

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('Mi App Segura funciona'));
+app.get('/', (req, res) => res.send('Mi App vers 2'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on ${port}`));
