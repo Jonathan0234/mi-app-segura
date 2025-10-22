b3d259d0376b3c6c108f1b03ecc8586ed1d8aa1c
JonathanArriaga0901 <jarriagac2@miumg.edu.gt>
2025-10-21 21:57:51 -0600

    pipeline ver2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('Mi App vers 2'));
+app.get('/', (req, res) => res.send('pipeline'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on ${port}`));
